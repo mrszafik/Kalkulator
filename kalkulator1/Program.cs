b80fa0c2055a71b811f5b55601a0c43d65687419
Maciej Szafrański <Maciej Szafraski>
2017-11-15 15:39:45 +0100

    Add project files.

---
+++ kalkulator1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace kalkulator1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
